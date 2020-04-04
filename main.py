3ed50fea0bdaf67dba0466a6689f3c6da14b34d8
toby <tjpatt@rams.colostate.edu>
2020-04-03 22:44:26 -0600

    Added some test code to main to display how the determinate function works.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 # Main file
+
+# code to dispaly how to use determinate
+from matrix_inverse import InverseCalculator
+
+ic = InverseCalculator("1 2 3; 4 5 6; 7 8 9")
+print(ic)
+print(ic.determinate())
